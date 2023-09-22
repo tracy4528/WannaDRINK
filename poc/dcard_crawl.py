8e6a70b401d014a0dc6cc47f0b340612e423f8ab
Tracy <tracy4528@gmail.com>
2023-09-22 16:41:51 +0800

    fix dcard crawl to lambda

--- poc/dcard_crawl.py
+++ poc/dcard_crawl.py
@@ -2,14 +2,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
 import time
-import random
 import undetected_chromedriver as uc
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import boto3
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+import pymysql
 load_dotenv()
 
 s3 = boto3.client('s3',
@@ -17,32 +19,39 @@ s3 = boto3.client('s3',
                     aws_access_key_id=os.getenv('iam_drink_key'),
                     aws_secret_access_key=os.getenv('iam_drink_secretkey'))
 
-driver = uc.Chrome()
+conn = pymysql.connect(host=os.getenv('mysql_host'), 
+                       user=os.getenv('mysql_user'),
+                       password=os.getenv('mysql_password'), 
+                       database='product')
+cursor = conn.cursor()
+
+driver = uc.Chrome(headless=True)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+
 
 driver.get("https://www.dcard.tw/f/boba")
 
-time.sleep(5)
 unique_urls = set()
 
 def get_dcard_articles():
     try:
-        for _ in range(10):
+        for _ in range(2):
             wait = WebDriverWait(driver, 20)           
             articles = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "article")))
             for article in articles:
+                print(article)
                 url = article.find_element(By.TAG_NAME, "a").get_attribute("href")
                 unique_urls.add(url)
             driver.execute_script("window.scrollBy(0,600);")
             time.sleep(5)
         unique_urls_list = list(unique_urls)
-        # print(unique_urls_list)
-        # print(f'===unique_urls===\n{unique_urls}')
-
 
     except Exception as e:
         print("error:", e)
     driver.quit()
-    # print(f'===final===\n{unique_urls_list}')
+    print(unique_urls_list)
     return unique_urls_list
 
 
@@ -51,12 +60,11 @@ def get_dcard_articles():
 
 def dcard_post_comment(dcard_topic_page):
     comments = []
-    
     driver.get(dcard_topic_page)
-    wait = WebDriverWait(driver, 3)
+    WebDriverWait(driver, 3)
     title=driver.find_element(By.CSS_SELECTOR, "h1[class='atm_w6_1hnarqo atm_7l_1e0g2gl atm_cs_b2t90y atm_c8_xi843o atm_g3_16tj23q bipaz8j']")
     content=driver.find_element(By.CSS_SELECTOR, "div[class='atm_c8_exct8b atm_g3_1f4h9lt atm_7l_1u09hbr c1h57ajp']")
-    
+    # article_time=driver.find_element(By.CSS_SELECTOR, "time[class='atm_7l_1w35wrm atm_vv_1q9ccgz atm_sq_1l2sidv atm_ks_15vqwwr atm_11ec84c_17s1f44 atm_5oq6dp_awxwf5 ifw06em']")
     driver.execute_script("window.scrollBy(0,500);")
 
     for _ in range(10):
@@ -75,14 +83,17 @@ def dcard_post_comment(dcard_topic_page):
         'comments': comments,
     }
     
+    print(data)
     return data
 
 
 
 if __name__ == "__main__":
     # article_list=get_dcard_articles()
+    article_list=['https://www.dcard.tw/f/boba/p/253356515', 'https://www.dcard.tw/f/boba/p/253353184']
+    
+
     today_date = datetime.now().strftime("%Y%m%d")
-    article_list=['https://www.dcard.tw/f/boba/p/253309217', 'https://www.dcard.tw/f/boba/p/253306912']
     dcard_post_comment(article_list[0])
 
     # for article in article_list:
