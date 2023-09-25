5d9f8937eb8fb491eba13eea937e11108902e26b
Tracy <tracy4528@gmail.com>
2023-09-25 19:56:38 +0800

    add drink and store to dict

--- rs/keyword_ptt.py
+++ rs/keyword_ptt.py
@@ -21,13 +21,36 @@ s3_bucket_name='wannadrink'
 s3_folder_path = 'ptt/20230918/'
 s3_object_key='ptt/20230918/1694500485.json'
 
+
+conn = pymysql.connect(host=os.getenv('mysql_host'), 
+                       user=os.getenv('mysql_user'),
+                       password=os.getenv('mysql_password'), 
+                       database='product')
+cursor = conn.cursor()
+
+
 # s3_object = s3.get_object(Bucket=s3_bucket_name, Key=s3_object_key)
 # json_data = s3_object['Body'].read().decode('utf-8')
 # data = json.loads(json_data)
 
 comments = []
 
-# 遍歷 S3 存儲桶中指定日期下的所有 JSON 文件
+
+def dict():
+    sql='SELECT original_name FROM foodpanda_store_code;'
+    cursor.execute(sql)
+    names = cursor.fetchall()
+    sql_p='SELECT name FROM foodpanda;'
+    cursor.execute(sql_p)
+    products = cursor.fetchall()
+    with open('dict.txt', 'w', encoding='utf-8') as f:
+        for name in names:
+            f.write(name[0] + '\n')
+    with open('dict.txt', 'a', encoding='utf-8') as f:
+        for product in products:
+            f.write(product[0] + '\n')
+    cursor.close()
+
 def extract_comments_from_s3():
     objects = s3.list_objects_v2(Bucket=s3_bucket_name, Prefix=s3_folder_path)
     for obj in objects.get('Contents'):
@@ -41,24 +64,33 @@ def extract_comments_from_s3():
                 comments.extend([comment['content'] for comment in data['comments']])
             if 'content' in data:
                 comments.extend([data['content']])
-    print(comments)
-
-
+            
 extract_comments_from_s3()
-# jieba.set_dictionary('dict.txt.big') 
-# jieba.load_userdict('./dict.txt_drink.txt')
+
+jieba.load_userdict('./dict_big.txt')
+jieba.analyse.set_stop_words('./stops.txt')
+comment_text = ' '.join(comments).encode('utf-8').decode('utf-8')
+tags=jieba.analyse.extract_tags (comment_text,topK=30, withWeight=False, allowPOS=())
+print(tags)
+
+'''
+jieba.set_dictionary('dict.txt.big') 
+jieba.load_userdict('./dict.txt_drink.txt')
 # with open('stops.txt', 'r', encoding='utf8') as f:
 #     stops = f.read().split('\n') 
 
 
-# comment_text = ' '.join(comments).encode('utf-8').decode('utf-8')
+comment_text = ' '.join(comments).encode('utf-8').decode('utf-8')
+jieba.analyse.set_stop_words('./dict.txt_drink.txt')
 # words = ' '.join(jieba.cut(comment_text))
+tags=jieba.analyse.extract_tags (comment_text,topK=10, withWeight=False, allowPOS=())
+print(tags)
 
-# stops.append('\n')  
-# stops.append('\n\n')
-# terms = [t for t in jieba.cut(words, cut_all=True) if t not in stops]
-# print(sorted(Counter(terms).items(), key=lambda x:x[1], reverse=True))
-'''
+stops.append('\n')  
+stops.append('\n\n')
+terms = [t for t in jieba.cut(words, cut_all=True) if t not in stops]
+print(sorted(Counter(terms).items(), key=lambda x:x[1], reverse=True))
+
 myWordClode = WordCloud(font_path='SourceHanSansTW-Regular.otf', width=800, height=400).generate(words)
 
 plt.figure(figsize=(8, 6), dpi=100) 
