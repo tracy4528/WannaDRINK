a92bac0e8e2784eb15ab9d4d98647cf2d002345b
Tracy <tracy4528@gmail.com>
2023-09-27 00:01:55 +0800

    add dcard_crawl to dockerfile

--- dcard_crawl_lambda/main.py
+++ dcard_crawl_lambda/main.py
@@ -1,119 +1,138 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import json 
+import time
+import undetected_chromedriver as uc
 import boto3
-import os
 from dotenv import load_dotenv
-import requests
-from bs4 import BeautifulSoup
-import json
+import os
 from datetime import datetime
+import pymysql
 load_dotenv()
 
 s3 = boto3.client('s3',
                     region_name='ap-northeast-1',
                     aws_access_key_id=os.getenv('iam_drink_key'),
                     aws_secret_access_key=os.getenv('iam_drink_secretkey'))
-article_list = []
-article_list1=[]
 
-def get_resp(url):
-    cookies = {
-        'over18': '1'
-    }
-    resp = requests.get(url, cookies=cookies)
-    if resp.status_code != 200:
-        return 'error'
-    else:
-        return resp
 
-def get_articles(resp):
-    soup = BeautifulSoup(resp.text, 'html5lib')
-    arts = soup.find_all('div', class_='r-ent')
-    for art in arts:
-        title = art.find('div', class_='title').getText().strip()
-        if not title.startswith('(本文已被刪除)') or title.startswith('[公告]'):
-            link = 'https://www.ptt.cc' + \
-                art.find('div', class_='title').a['href'].strip()
-        author = art.find('div', class_='author').getText().strip()
-        article_code=art.find('div', class_='title').a['href'].strip().split('/')[-1].split('.')[1]
-        article = {
-            'title': title,
-            'link': link,
-            'author': author,
-            'article_code':article_code
-        }
-        article_list.append(article)
+conn = pymysql.connect(host=os.getenv('mysql_host'), 
+                       user=os.getenv('mysql_user'),
+                       password=os.getenv('mysql_password'), 
+                       database='product')
+cursor = conn.cursor()
+
+driver = uc.Chrome()
+
+
+
+driver.get("https://www.dcard.tw/f/boba")
+today_date = datetime.now().strftime("%Y%m%d")
+
+unique_urls=set()
+unique_urls_list = []
+def get_dcard_articles():
+    try:
+        articles_data = []  # 用來儲存要插入的數據
+        for _ in range(1):
+            wait = WebDriverWait(driver, 20)
+            articles = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "article")))
+            for article in articles:
+                url = article.find_element(By.TAG_NAME, "a").get_attribute("href")
+                title_element = article.find_element(By.TAG_NAME, "h2")
+                title = title_element.get_attribute("innerText")  # 使用 get_attribute 取得標題文本
+                like_num = article.find_element(By.CSS_SELECTOR, "div[class='atm_lk_i2wt44 c1jkhqx5']")
+                push = like_num.text
+
+                unique_urls.add(url)
+                articles_data.append((title, url, push, today_date))  # 將數據添加到列表中
+
+            driver.execute_script("window.scrollBy(0,600);")
+            time.sleep(5)
+
+        print(set(articles_data))
+
+        unique_articles_data=set(articles_data)
         
-    next_url = 'https://www.ptt.cc' + \
-        soup.select_one('#action-bar-container > div > div.btn-group.btn-group-paging > a:nth-child(2)')['href']
-    
-    return next_url
+        insert_product_sql = ("INSERT INTO `dcard_articles` (title, url, push, crawl_date) VALUES (%s, %s, %s, %s)")
+        cursor = conn.cursor()
+        cursor.executemany(insert_product_sql, unique_articles_data)
+        conn.commit()
+            
+        unique_urls_list = list(unique_urls)
+        print("=========")
+        print(unique_urls_list)
+        
+    except Exception as e:
+        print("error:", e)
 
-def get_post_comment(url):
-    
+        
+    return unique_urls_list
 
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, 'html.parser')
 
-    meta_data = {
-        'author': soup.find('div', {'class': 'article-metaline'}).find('span', {'class': 'article-meta-value'}).text,
-        'title': soup.find_all('div', {'class': 'article-metaline'})[1].find('span', {'class': 'article-meta-value'}).text,
-        'date': soup.find_all('div', {'class': 'article-metaline'})[2].find('span', {'class': 'article-meta-value'}).text,
-    }
 
-    content = soup.find('div', {'id': 'main-content'}).text
 
+
+
+def dcard_post_comment(dcard_topic_page):
     comments = []
-    for comment in soup.find_all('div', {'class': 'push'}):
-        push_tag = comment.find('span', {'class': 'push-tag'}).text.strip()
-        push_userid = comment.find('span', {'class': 'push-userid'}).text.strip()
-        push_content = comment.find('span', {'class': 'push-content'}).text.strip()
-        push_time = comment.find('span', {'class': 'push-ipdatetime'}).text.strip()
+    driver.get(dcard_topic_page)
+    WebDriverWait(driver, 3)
+    title=driver.find_element(By.CSS_SELECTOR, "h1[class='atm_w6_1hnarqo atm_7l_1e0g2gl atm_cs_b2t90y atm_c8_xi843o atm_g3_16tj23q bipaz8j']")
+    content=driver.find_element(By.CSS_SELECTOR, "div[class='atm_c8_exct8b atm_g3_1f4h9lt atm_7l_1u09hbr c1h57ajp']")
+    # article_time=driver.find_element(By.CSS_SELECTOR, "time[class='atm_7l_1w35wrm atm_vv_1q9ccgz atm_sq_1l2sidv atm_ks_15vqwwr atm_11ec84c_17s1f44 atm_5oq6dp_awxwf5 ifw06em']")
+    driver.execute_script("window.scrollBy(0,500);")
+
+    for _ in range(2):
+        # driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+        driver.execute_script("window.scrollBy(0,200);")
+        commant=driver.find_elements(By.CSS_SELECTOR,"div[class='atm_7l_1u09hbr atm_c8_exct8b atm_g3_1f4h9lt c201erw']")
+    for com in commant:
         comments.append({
-            'tag': push_tag,
-            'userid': push_userid,
-            'content': push_content,
-            'time': push_time,
+            'content': com.text,
         })
 
     data = {
-        'meta_data': meta_data,
-        'content': content,
+        'title': title.accessible_name,
+        'content': content.text,
         'comments': comments,
     }
+    
+    return data
 
-    json_data = json.dumps(data, ensure_ascii=False, indent=4)
 
-    return json_data
+    
+
 
 def handler(event=None, context=None):
-    url = 'https://www.ptt.cc/bbs/Drink/index.html'
-    today_date = datetime.now().strftime("%Y%m%d")
+    article_list=get_dcard_articles()
+    # article_list=[ 'https://www.dcard.tw/f/boba/p/253353184','https://www.dcard.tw/f/boba/p/253356515']
+    
 
-    for now_page_number in range(1):
-        print(f'crawing {url}')
-        resp = get_resp(url)
-        if resp != 'error':
-            url = get_articles(resp)
-        print(f'======={now_page_number+1}/10=======')
+    today_date = datetime.now().strftime("%Y%m%d")
     
     for article in article_list:
         try:
-            print(article['title'])
-            json_data=get_post_comment(article['link'])
-            article_code=article['article_code']
-            name=article['title']
+            print("=======start=======")
+            json_data=dcard_post_comment(article)
+            json_bytes = json.dumps(json_data, ensure_ascii=False, indent=4)
+            article_code=article.split('/')[-1]
             bucket_name = 'wannadrink'
-            s3_object_key=f'ptt/{today_date}/{article_code}.json'
-            folder_path = os.path.join('ptt/', today_date)
+            s3_object_key=f'dcard/{today_date}/{article_code}.json'
+            folder_path = os.path.join('dcard/', today_date)
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
             else:
                 s3.put_object(
                     Bucket=bucket_name,
                     Key=s3_object_key,
-                    Body=json_data,
+                    Body=json_bytes,
                     ContentType='application/json'
-            )
-            print(f'======={name}=======')
+                )
+            print(f'======={article_code}=======')
         except Exception as e:
             print(f'Error encountered: {e}')
             continue
