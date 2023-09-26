ebc7fe4290c2de2bf5f3943e58eb0e49383c0366
Tracy <tracy4528@gmail.com>
2023-09-26 15:52:33 +0800

    use openai get ptt hot word

--- rs/user_rs_openai.py
+++ rs/user_rs_openai.py
@@ -1,16 +1,64 @@
 import openai
+from dotenv import load_dotenv
+from datetime import datetime
+import json 
 
-openai.api_key = "<你的 API Key>" 
-audio_file= open("cropped.mp3", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-text = transcript.to_dict()['text']
+import os
+import pymysql
+import boto3
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "說句話吧"}
-    ]
-)
 
-print(text)
-#抓當天熱門文章喂給onenapi 抓關鍵字回來
+load_dotenv()
+
+
+conn = pymysql.connect(host=os.getenv('mysql_host'), 
+                       user=os.getenv('mysql_user'),
+                       password=os.getenv('mysql_password'), 
+                       database='product',
+                       cursorclass=pymysql.cursors.DictCursor)
+cursor = conn.cursor()
+s3 = boto3.client('s3',
+                    region_name='ap-northeast-1',
+                    aws_access_key_id=os.getenv('iam_drink_key'),
+                    aws_secret_access_key=os.getenv('iam_drink_secretkey'))
+
+comments = []
+today_date = datetime.today().strftime('%Y%m%d')
+
+def hot_article_text_ptt():
+    sql=f"SELECT url FROM ptt_articles  where crawl_date='{today_date}' ORDER BY push DESC limit 1"
+    cursor.execute(sql)
+    urls = cursor.fetchall()
+    for url in urls:
+        data=url['url'].split('/')[-1].split('.')[1]
+        file_key= 'ptt/'+today_date+'/'+data+'.json'
+        response = s3.get_object(Bucket='wannadrink', Key=file_key)
+        json_data = response['Body'].read().decode('utf-8')
+        data = json.loads(json_data)
+
+        if 'comments' in data:
+                comments.extend([comment['content'] for comment in data['comments']])
+        if 'content' in data:
+                comments.extend([data['content']])
+    return comments
+
+
+def get_hot_word():
+    text=hot_article_text_ptt()
+
+    openai.api_key = os.getenv('openai_key')
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"請幫我從以下論壇文章取出兩個熱門討論的飲料店及飲料品項, 文章：'{text}'"}
+        ]
+    )
+    keyword=completion["choices"][0]["message"]["content"]
+    print(keyword)
+
+    insert_sql = ("INSERT INTO `hot_keyword` (keyword, date, from_) VALUES (%s, %s, %s)")
+    cursor = conn.cursor()
+    cursor.execute(insert_sql, (keyword,today_date,'ptt'))  
+    conn.commit()
+
+
