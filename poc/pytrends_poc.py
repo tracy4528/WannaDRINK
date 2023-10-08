b8705b536d86c463844232ba7a7f6cea188367dc
Tracy <tracy4528@gmail.com>
2023-10-08 22:17:52 +0800

    add 14 store google trend

--- poc/pytrends_poc.py
+++ poc/pytrends_poc.py
@@ -4,6 +4,7 @@ import pymysql
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+import time
 
 load_dotenv()
 
@@ -13,14 +14,30 @@ conn = pymysql.connect(host=os.getenv('mysql_host'),
                        database='product')
 cursor = conn.cursor()
 
-
 pytrend = TrendReq(hl='en-US', tz=360)
-keywords = ['可不可', '清心福全', '茶湯會','迷客夏','一沐日']
-pytrend.build_payload(
-     kw_list=keywords,
-     cat=0,
-     timeframe='today 1-m',
-     geo='TW',
-     gprop='')
+keywords=['WooTea五桐號','清心福全','可不可']
+# keywords = [ '茶湯會','迷客夏','一沐日','再睡5分鐘','五十嵐','茶的魔手',
+#             '大苑子','龜記','五桐號','不要對我尖叫','珍煮丹','大茗,'烏弄','coco都可']
 
-pprint(pytrend.interest_over_time())
+for keyword in keywords:
+     print(keyword)
+     time.sleep(10)
+     pytrend.build_payload(
+          kw_list=keywords,
+          cat=0,
+          timeframe='today 3-m',
+          geo='TW',
+          gprop='')
+     
+interest_over_time_df = pytrend.interest_over_time()
+for keyword in keywords:
+     print(keyword)
+     for index, row in interest_over_time_df.iterrows():
+          insert_product_sql = ("INSERT INTO `google_trend` (store, trend_date,trend_num) "
+                                             "VALUES (%s,%s,%s)")
+          cursor.execute(insert_product_sql,(keyword,index,row[keyword]) )
+     conn.commit() 
+
+
+
+
